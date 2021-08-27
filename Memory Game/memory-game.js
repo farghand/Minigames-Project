4e5781dc8e191a0d92dcd72404b9c62799e0d982
Fayaz Arghandehwal <fayaz.arghandehwal@yahoo.com>
2021-08-27 14:41:52 -0700

    Restructuring files to separate each game by folder

---
+++ Memory Game/memory-game.js
@@ -0,0 +1,6 @@
+function onMainButton() {
+    let mainButton = document.getElementById("main-menu-button");
+    mainButton.onclick = function() {
+        location.href = "../index.html";
+    }
+}
